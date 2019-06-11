45d70c74ff5543ca5ea9db0d6f1df96f36619020
Edison Devadoss <edisonj1996@gmail.com>
2019-06-11 11:37:19 +0530

    Add more queries

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get("/", (req, res) => {
 });
 
 app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
 app.post('/users/', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.listen(PORT);
--- queries.js
+++ queries.js
@@ -1,6 +1,7 @@
 const { Client } = require("pg");
 
-const connectionString = 'postgres://wtmjhuyuspqgfn:b535b72f5bc09e7951dd562e7a271d62fa00f3558a7fccfe358350131b2a0b69@ec2-23-21-156-171.compute-1.amazonaws.com:5432/d20glbcf4n5itl'
+const connectionString =
+  "postgres://wtmjhuyuspqgfn:b535b72f5bc09e7951dd562e7a271d62fa00f3558a7fccfe358350131b2a0b69@ec2-23-21-156-171.compute-1.amazonaws.com:5432/d20glbcf4n5itl";
 
 const client = new Client({
   connectionString: connectionString,
@@ -18,6 +19,17 @@ const getUsers = (request, response) => {
   });
 };
 
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  client.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
 const createUser = (request, response) => {
   const { name, email } = request.body;
 
@@ -28,10 +40,37 @@ const createUser = (request, response) => {
       if (error) {
         throw error;
       }
-      console.log('request', request);
+      console.log("request", request);
       response.status(200).send(`User added with ID: ${result.id}`);
     }
   );
 };
 
-module.exports = { getUsers, createUser };
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  client.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  client.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = { getUsers, createUser, updateUser, getUserById, deleteUser };
