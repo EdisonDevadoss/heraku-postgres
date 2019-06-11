365dbcc9f36dd3c9d950b3dddd22103173a6642c
Edison Devadoss <edisonj1996@gmail.com>
2019-06-11 10:27:40 +0530

    Add nodemon

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = proccess.NODE.env || 3000
+const port = process.env.NODE_ENV || 3000;
+
+app.get('/', (req, res)=>{
+  res.json({message: 'test fetch'})
+});
+
 
 app.listen(port);
